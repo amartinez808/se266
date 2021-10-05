918984e748193cf55a6ec55b95df682d9cc0a57f
amartinez808 <amartinez@email.neit.edu>
2021-10-05 18:10:56 -0400

    w1 second assgmt.

---
+++ w1/index2.php
@@ -0,0 +1,12 @@
+<?php
+
+$task = [
+    
+    'title' => 'finish homework',
+    'due' => 'today',
+    'assigned_to' => 'Antonio',
+    'completed' => false 
+    
+];
+
+require 'index.view.php';
