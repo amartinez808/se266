1cd0710ef450c748d2329e23581cacb6405f4c4a
amartinez808 <amartinez@email.neit.edu>
2021-10-05 13:32:43 -0400

    se266 week1.

---
+++ w1/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$animals = [ 
+    'hyppo',
+    'jirafe',
+    'dog',
+    'cat',
+    'bird'
+];
+
+
+#foreach ($animals as $animals) {
+
+    #echo $animals.', ';
+
+#}
+
+
+
+
+
+require 'index.view.php';
---
+++ w1/index.view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+
+    <style> 
+        header{
+            background: #e3e3e3;
+            padding:2em;
+            text-align: center;
+        }
+
+    
+    </style>
+</head>
+
+<body>
+    <ul> 
+        <?php 
+        foreach ($animals as $animal) {
+                echo "<li>$animal</li>";
+
+            }
+        
+        ?>
+
+
+
+    </ul>
+
+
+    
+</body>
+
+
+
+</html>
