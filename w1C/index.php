2a83da7bf6e1b6c2161f968d36fc1116033ebd70
amartinez808 <amartinez@email.neit.edu>
2021-10-10 12:57:11 -0400

    w1C.

---
+++ w1C/index.php
@@ -0,0 +1,15 @@
+<?php
+require 'functions.php';
+
+require 'index.view.php';
+$task = [
+    
+    'title' => 'finish homework',
+    'due' => 'today',
+    'assigned_to' => 'Antonio',
+    'completed' => false
+];
+dd($task);
+
+
+?>
